29917aabfc79751cb0a3e163a4dc2a378132c94d
Alex Naidis <alex.naidis@linux.com>
2017-03-06 22:03:58 +0100

    Introduce FP unlock boost
    This patch adds a boost which is meant to speed up
    unlocking using the fingerprint sensor while the screen
    is off.
    
    So it boosts once successful fingerprint authentication
    happened while the screen was off.
    
    To minimize the configuration efforts, this re-uses the strong
    boost parameters (boost pattern) from Keypress Boost.
    
    The boost duration can be specified via an overlay and
    defaults to 1200ms.
    
    Change-Id: Ibd8d7e20d26c0014cbfe10d8ce7cd42a0a9f7081
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: Joe Maples <joe@frap129.org>

--- packages/Keyguard/src/com/android/keyguard/KeyguardUpdateMonitor.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardUpdateMonitor.java
@@ -66,6 +66,7 @@ import android.telephony.SubscriptionManager;
 import android.telephony.SubscriptionManager.OnSubscriptionsChangedListener;
 import android.telephony.TelephonyManager;
 import android.util.ArraySet;
+import android.util.BoostFramework;
 import android.util.Log;
 import android.util.SparseBooleanArray;
 import android.util.SparseIntArray;
@@ -212,6 +213,32 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
     private TrustManager mTrustManager;
     private UserManager mUserManager;
     private int mFingerprintRunningState = FINGERPRINT_STATE_STOPPED;
+
+    private BoostFramework mPerf = null;
+    private boolean lIsPerfBoostEnabled;
+    private int[] mBoostParamVal;
+    private int mBoostDuration;
+
+    private PocketManager mPocketManager;
+    private boolean mIsDeviceInPocket;
+    private final IPocketCallback mPocketCallback = new IPocketCallback.Stub() {
+        @Override
+        public void onStateChanged(boolean isDeviceInPocket, int reason) {
+            boolean changed = false;
+            if (reason == PocketManager.REASON_SENSOR) {
+                if (isDeviceInPocket != mIsDeviceInPocket) {
+                    mIsDeviceInPocket = isDeviceInPocket;
+                    changed = true;
+                }
+            } else {
+                changed = isDeviceInPocket != mIsDeviceInPocket;
+                mIsDeviceInPocket = false;
+            }
+            if (changed) {
+                mHandler.sendEmptyMessage(MSG_POCKET_STATE_CHANGED);
+            }
+        }
+    };
 
     private final Handler mHandler = new Handler() {
         @Override
@@ -455,6 +482,13 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
         // wake-up (if Keyguard is not showing), so we don't need to listen until Keyguard is
         // fully gone.
         mFingerprintAlreadyAuthenticated = isUnlockingWithFingerprintAllowed();
+
+        // Intercept the authorized FP unlock while the screen is off
+        if (lIsPerfBoostEnabled && !mScreenOn) {
+            Log.i(TAG, "Dispatching FP unlock boost.");
+            mPerf.perfLockAcquire(mBoostDuration, mBoostParamVal);
+        }
+
         for (int i = 0; i < mCallbacks.size(); i++) {
             KeyguardUpdateMonitorCallback cb = mCallbacks.get(i).get();
             if (cb != null) {
@@ -1146,6 +1180,20 @@ public class KeyguardUpdateMonitor implements TrustManager.TrustListener {
                     }, TAG);
         } catch (RemoteException e) {
             e.rethrowAsRuntimeException();
+        }
+
+        // Initialise FP unlock boost
+        lIsPerfBoostEnabled = mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableKeypressBoost) &&
+                mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableFPUnlockBoost);
+        mBoostParamVal = mContext.getResources().getIntArray(
+                com.android.internal.R.array.keypressboost_strong_param_value);
+        mBoostDuration = mContext.getResources().getInteger(
+                com.android.internal.R.integer.fpunlockboost_duration);
+
+        if (lIsPerfBoostEnabled) {
+            mPerf = new BoostFramework();
         }
 
         IntentFilter strongAuthTimeoutFilter = new IntentFilter();
